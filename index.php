8d4eb9f06312ce2fb834df5ea6a52bb855ebca53
Cincinn4tus <aligoumane@protonmail.com>
2023-04-14 09:58:17 +0200

    index

--- index.php
+++ index.php
@@ -5,17 +5,39 @@
 
 <div class="container-fluid" id="hero">
   <div id="hero-cover">
-    <div class="row text-center mt-5" id="hero-content">
+    <div class="row text-center mt-5 col-lg-8" id="hero-content">
       <h1>Financer l'innovation ensemble</h1>
 
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta harum accusamus ratione id similique. Quia, assumenda ipsum et commodi alias vero maxime accusantium, optio consequuntur at beatae quas sunt nisi.
       </p>
-
       <a href="" class="btn btn-get-started">Par ici</a>
     </div>
   </div>
 </div>
 
+
+<section id="possibilities">
+			<div class="wrapper">
+                <article style="background-image: url(images/article-image-1.jpg);">
+                    <div class="overlay">
+                        <h4>Partez en famille</h4>
+                        <p><small>Offrez le meilleur à ceux que vous aimez et partagez des moments fabuleux !</small></p>
+                        <a href="#" class="button-2">Plus d'infos</a>
+                    </div>
+                </article>
+                
+                <article style="background-image: url(images/article-image-2.jpg);">
+                    <div class="overlay">
+                        <h4>Envie de s'evader</h4>
+                        <p><small>Parfois un peu d'évasion serait le bienvenue et ferait le plus grand bien !</small></p>
+                        <a href="#" class="button-2">Plus d'infos</a>
+                    </div>
+                </article>
+                
+                <div class="clear"></div>
+                
+			</div>
+		</section>
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/assets/templates/footer.php"; ?>
