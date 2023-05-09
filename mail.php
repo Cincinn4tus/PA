37b5acabc30c8923c358472e6acc35b460d081fc
Cincinn4tus <aligoumane@protonmail.com>
2023-05-09 10:44:31 +0200

    phpmailer link

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
 
-    require $_SERVER['DOCUMENT_ROOT'] . "/phpmailer/PHPMailer.php";
+    require $_SERVER['DOCUMENT_ROOT'] . "/phpmailer/src/PHPMailer.php";
 
 
 // Créer une nouvelle instance de PHPMailer
