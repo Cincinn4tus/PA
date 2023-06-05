c4bd2c6bbb546c4326af12e10918ce79c5c20966
Cincinn4tus <aligoumane@protonmail.com>
2023-06-06 00:24:16 +0200

    userAdd

--- core/userAdd.php
+++ core/userAdd.php
@@ -2,38 +2,33 @@
 session_start();
 require "functions.php";
 
+$listOfErrors = [];
 
-// Vérification des champs obligatoires
-if (empty($_POST['firstname']) 
-	|| empty($_POST['lastname'])
-	|| empty($_POST['email'])
-	|| empty($_POST['pwd'])
-	|| empty($_POST['pwdConfirm'])
-	|| empty($_POST['phone_number'])
-	|| empty($_POST['cgu']) //check if checkbox is checked
-	|| (!isset($_POST['EntrepreneurCheck']) && !isset($_POST['InvestisseurCheck']))
-){
-	die ("Tentative de HACK");
+// Fields common to both Entrepreneur and Investor
+$requiredFields = ['firstname', 'lastname', 'email', 'pwd', 'pwdConfirm', 'phone_number', 'cgu'];
+
+// Fields specific to Entrepreneur
+$entrepreneurFields = ['Phone_numberEE', 'SiretEE', 'nameEntrepriseEE'];
+
+// Fields specific to Investor
+$investorFields = ['Phone_numberEI', 'SiretEI', 'nameEntrepriseEI'];
+
+$roleFields = [];
+
+// Determine the role of the user and update the list of required fields accordingly
+if (isset($_POST['EntrepreneurCheck'])) {
+    $roleFields = $entrepreneurFields;
+} elseif (isset($_POST['InvestisseurCheck'])) {
+    $roleFields = $investorFields;
+} else {
+    die("Tentative de HACK");
 }
 
-// Vérification des champs spécifiques en fonction de si l'utilisateur est un entrepreneur ou un investisseur
-if (!empty($_POST['EntrepreneurCheck']) 
-    && (
-        empty($_POST['Phone_numberE']) 
-    	|| empty($_POST['Siret']) 
-    	|| empty($_POST['nameEntreprise']) 
-    )
-) {
-	die ("Tentative de HACK");
-}
-
-if (!empty($_POST['InvestisseurCheck']) 
-    && (
-        empty($_POST['projetsInvestis']) 
-        || empty($_POST['projetFait'])
-    )
-) {
-	die ("Tentative de HACK");
+// Check if all required fields are present
+foreach (array_merge($requiredFields, $roleFields) as $field) {
+    if (empty($_POST[$field])) {
+        die("Tentative de HACK");
+    }
 }
 
 //Nettoyage des données
@@ -48,9 +43,14 @@ $phone_number = $_POST['phone_number'];
 $projetFait = $_POST['projetFait'];
 $projetsInvestis = $_POST['projetsInvestis'];
 
-$phone_numberE = $_POST['Phone_numberE'];
-$siret = $_POST['Siret'];
-$nameEntreprise = $_POST['nameEntreprise'];
+$Phone_numberEE = $_POST['Phone_numberEE'];
+$SiretEE = $_POST['SiretEE'];
+$nameEntrepriseEE = $_POST['nameEntrepriseEE'];
+
+$Phone_numberEI = $_POST['Phone_numberEI'];
+$SiretEI = $_POST['SiretEI'];
+$nameEntrepriseEI = $_POST['nameEntrepriseEI']; 
+
 $entrepreneurCheck = $_POST['EntrepreneurCheck'];
 $investisseurCheck = $_POST['InvestisseurCheck'];
 
@@ -120,27 +120,36 @@ if(!isset($_POST['captcha_solved']) || $_POST['captcha_solved'] != '1'){
 
 //Si OK
 
-	if (empty($listOfErrors)) {
-	
-		$queryPrepared = $connection->prepare("INSERT INTO ".DB_PREFIX."user
-												(firstname, lastname, email, pwd, phone_number, projetsInvestis, projetFait, phone_numberE, siret, nameEntreprise, entrepreneurCheck, investisseurCheck)
-												VALUES 
-												(:firstname, :lastname, :email, :pwd, :phone_number, :projetsInvestis, :projetFait, :phone_numberE, :siret, :nameEntreprise, :entrepreneurCheck, :investisseurCheck)");
-												$queryPrepared->execute([
-													"firstname" => $firstname,
-													"lastname" => $lastname,
-													"email" => $email,
-													"pwd" => password_hash($pwd, PASSWORD_DEFAULT),
-													"phone_number" => $phone_number,
-													"projetsInvestis" => $projetsInvestis,
-													"projetFait" => $projetFait,
-													"phone_numberE" => $phone_numberE,
-													"siret" => $siret,
-													"nameEntreprise" => $nameEntreprise,
-													"entrepreneurCheck" => $entrepreneurCheck, // add these
-													"investisseurCheck" => $investisseurCheck, // add these
-													"cgu" => $cgu // add this
-												]);
+if (empty($listOfErrors)) {
+    // Determine if the user is an entrepreneur or an investor and save data accordingly
+    if (isset($_POST['EntrepreneurCheck'])) {
+        $phone_numberE = $Phone_numberEE;
+        $siret = $SiretEE;
+        $nameEntreprise = $nameEntrepriseEE;
+    } elseif (isset($_POST['InvestisseurCheck'])) {
+        $phone_numberE = $Phone_numberEI;
+        $siret = $SiretEI;
+        $nameEntreprise = $nameEntrepriseEI;
+    }
+
+    $queryPrepared = $connection->prepare("INSERT INTO ".DB_PREFIX."user
+        (firstname, lastname, email, pwd, phone_number, projetsInvestis, projetFait, phone_numberE, siret, nameEntreprise, entrepreneurCheck, investisseurCheck)
+        VALUES 
+        (:firstname, :lastname, :email, :pwd, :phone_number, :projetsInvestis, :projetFait, :phone_numberE, :siret, :nameEntreprise, :entrepreneurCheck, :investisseurCheck)");
+    $queryPrepared->execute([
+        "firstname" => $firstname,
+        "lastname" => $lastname,
+        "email" => $email,
+        "pwd" => password_hash($pwd, PASSWORD_DEFAULT),
+        "phone_number" => $phone_number,
+        "projetsInvestis" => $projetsInvestis ?? null, // if not set, set to null
+        "projetFait" => $projetFait ?? null, // if not set, set to null
+        "phone_numberE" => $phone_numberE,
+        "siret" => $siret,
+        "nameEntreprise" => $nameEntreprise,
+        "entrepreneurCheck" => $entrepreneurCheck,
+        "investisseurCheck" => $investisseurCheck,
+    ]);
 
 		//Redirection sur la page de connexion
 		header('location: ../user/login.php');
