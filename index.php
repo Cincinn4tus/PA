1b72af839159ac6691a8e9b20bd9b1a6433e9b27
Cincinn4tus <aligoumane@protonmail.com>
2023-06-25 20:00:07 +0200

    index

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="hero">
     <div class="info d-flex align-items-center">
-      <div class="container">
+
         <div class="row justify-content-center">
           <div class="col-lg-6 text-center">
             <h2 data-aos="fade-down">Bienvenue sur <span>CrowdHub</span></h2>
@@ -18,7 +18,7 @@
             <a data-aos="fade-up" data-aos-delay="200" href="#get-started" class="btn-get-started">Commencer</a>
           </div>
         </div>
-      </div>
+
     </div>
 
     <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
