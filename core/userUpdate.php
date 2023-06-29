b5984496d017ab69e0c45d7df2c930c6e7a96685
Cincinn4tus <aligoumane@protonmail.com>
2023-06-29 07:13:17 +0200

    admin

--- core/userUpdate.php
+++ core/userUpdate.php
@@ -17,6 +17,8 @@
     $postal_code = $_POST["postal_code"];
     $email = $user["email"];
     $scope = $_POST["scope"];
+    $pwd = $user["pwd"];
+    $pwdConfirm = $_POST["pwdConfirm"];
 
     // nettoyage
     $id = trim(strip_tags($userId));
@@ -25,10 +27,20 @@
     $postal_code = trim(strip_tags($postal_code));
     $email = trim(strip_tags($email));
     $scope = trim(strip_tags($scope));
-    
+    $pwd = trim(strip_tags($pwd));
+    $pwdConfirm = trim(strip_tags($pwdConfirm));
+    if(!empty($pwd) && !empty($pwdConfirm)){
+        if($pwd == $pwdConfirm){
+            $pwd = password_hash($pwd, PASSWORD_DEFAULT);
+        } else {
+            echo "Les mots de passe ne correspondent pas";
+        }
+    } else {
+        $pwd = $user["pwd"];
+    }
 
     $pdo = connectDB();
-    $queryPrepared = $pdo->prepare("UPDATE ".DB_PREFIX."user SET phone_number=:phone, postal_address=:postal_address, postal_code=:postal_code, email=:email, scope=:scope WHERE id=:id");
+    $queryPrepared = $pdo->prepare("UPDATE ".DB_PREFIX."user SET phone_number=:phone, postal_address=:postal_address, postal_code=:postal_code, email=:email, scope=:scope, pwd=:pwd WHERE id=:id");
     $queryPrepared->execute([
         "phone"=>$phone,
         "postal_address"=>$address,
