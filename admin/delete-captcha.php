c20c96c2ea2163ad0b2a8faa8ce47a6e6a682b00
Cincinn4tus <aligoumane@protonmail.com>
2023-07-02 17:32:10 +0200

    captcha

--- admin/delete-captcha.php
+++ admin/delete-captcha.php
@@ -4,12 +4,16 @@
     require $_SERVER['DOCUMENT_ROOT'] . "/core/functions.php";
 
     if (isset($_POST['file-name'])) {
-        $file = $_POST['file-name'];
-        $path = $_SERVER['DOCUMENT_ROOT'] . "/assets/img/captcha/" . $file;
-        if(file_exists($path)) {
-            unlink($path);
-        } else {
-            echo "Le fichier $file n'existe pas dans le répertoire /assets/img/captcha/.";
+        try {
+            $file = $_POST['file-name'];
+            $path = $_SERVER['DOCUMENT_ROOT'] . "/assets/img/captcha/" . $file;
+            if(file_exists($path)) {
+                unlink($path);
+            } else {
+                echo "Le fichier $file n'existe pas dans le répertoire /assets/img/captcha/.";
+            }
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
         }
     }
 ?>
