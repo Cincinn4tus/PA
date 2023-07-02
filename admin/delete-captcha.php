908c0675ac563f48abe95efc65d0a56fc2b8316c
Cincinn4tus <aligoumane@protonmail.com>
2023-07-02 18:03:31 +0200

    captcha

--- admin/delete-captcha.php
+++ admin/delete-captcha.php
@@ -11,84 +11,18 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-?>
-<!-- Votre HTML reste le même -->
 
-<?php
-    $dir = $_SERVER['DOCUMENT_ROOT'] . "/assets/img/captcha/";
-    $files = scandir($dir);
-    $files = array_diff(scandir($dir), array('.', '..'));
-    // Reste du code ...
-?>
 
-<script type='text/javascript'>
-    // Votre script JS reste le même
-    function removeCaptcha(fileName) {
-        let formData = new FormData();
-        formData.append('file-name', fileName);
-
-        fetch('/admin/delete-captcha.php', {
-            method: 'POST',
-            body: formData
-        }).then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
+            // Vérifie si le fichier existe
+            if(file_exists($target_file)) {
+                // Supprime le fichier
+                if(unlink($target_file)) {
+                    echo "Le fichier " . $file_name . " a été supprimé.";
+                } else {
+                    echo "Désolé, une erreur s'est produite lors de la suppression du fichier.";
+                }
+            } else {
+                echo "Désolé, le fichier n'existe pas.";
             }
-            location.reload();
-        }).catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
-    }
-</script>
-
-<?php
-    include $_SERVER["DOCUMENT_ROOT"] . "/assets/templates/footer.php";
-    page_load_time();
+        
 ?>
-<?php
-// Start the session
-session_start();
-
-// Include necessary files
-require $_SERVER['DOCUMENT_ROOT'] . "/conf.inc.php";
-require $_SERVER['DOCUMENT_ROOT'] . "/core/functions.php";
-
-// Check if the filename is provided
-if (!isset($_POST['file-name'])) {
-    // Respond with an error status code
-    http_response_code(400);
-    exit;
-}
-
-// Get the filename
-$filename = $_POST['file-name'];
-
-// Validate the filename
-if (!isValidFileName($filename)) {
-    // Respond with an error status code
-    http_response_code(400);
-    exit;
-}
-
-// Define the directory where CAPTCHA images are stored
-$dir = $_SERVER['DOCUMENT_ROOT'] . "/assets/img/captcha/";
-
-// Define the path to the file
-$file_path = $dir . $filename;
-
-// Check if the file exists
-if (!file_exists($file_path)) {
-    // Respond with an error status code
-    http_response_code(404);
-    exit;
-}
-
-// Try to delete the file
-if (!unlink($file_path)) {
-    // Respond with an error status code
-    http_response_code(500);
-    exit;
-}
-
-// If everything went well, respond with a success status code
-http_response_code(200);
--- admin/upload-captcha.php
+++ admin/upload-captcha.php
@@ -7,13 +7,46 @@
     if(!isConnected() || $user['scope'] != 0){
         header("Location: /errors/403.php");
         }
+        ?>
 
     
-    if (isset($_FILES['captcha-file'])) {
-        $file = $_FILES['captcha-file'];
-        move_uploaded_file($file['tmp_name'], "../assets/img/captcha/" . $file['name']);
+<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $upload_dir = "../assets/img/captcha/";
+        $file_name = basename($_FILES["captcha-file"]["name"]);
+        $target_file = $upload_dir . $file_name;
+
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+        // Vérifie si le fichier est une véritable image
+        $check = getimagesize($_FILES["captcha-file"]["tmp_name"]);
+        if($check !== false) {
+            // Vérifie si le fichier existe déjà
+            if (!file_exists($target_file)) {
+                // Autorise certains formats de fichiers
+                if($imageFileType == "jpg"  $imageFileType == "png"  $imageFileType == "jpeg" || $imageFileType == "gif" ) {
+                    // Déplace le fichier de son emplacement temporaire vers le répertoire de destination
+                    if (move_uploaded_file($_FILES["captcha-file"]["tmp_name"], $target_file)) {
+                        echo "Le fichier ". $file_name. " a été uploadé.";
+                    } else {
+                        echo "Désolé, une erreur s'est produite lors de l'upload de votre fichier.";
+                    }
+                } else {
+                    echo "Désolé, seuls les fichiers JPG, JPEG, PNG & GIF sont autorisés.";
+                }
+            } else {
+                echo "Désolé, le fichier existe déjà.";
+            }
+        } else {
+            echo "Le fichier n'est pas une image.";
+        }
     }
 ?>
+
+
+
+
+
 <script>
     document.getElementById('captcha-form').addEventListener('submit', function(e) {
     e.preventDefault();
