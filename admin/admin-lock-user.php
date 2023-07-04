2820fdb2ddfd4a3c4054f9cbf1883ba908c558d2
Cincinn4tus <aligoumane@protonmail.com>
2023-07-04 08:29:33 +0200

    voir profil

--- admin/admin-lock-user.php
+++ admin/admin-lock-user.php
@@ -19,4 +19,28 @@ $id = $_GET["id"];
     ]);
 
 
+    require $_SERVER['DOCUMENT_ROOT'] . '/PHPMailer/src/Exception.php';
+    require $_SERVER['DOCUMENT_ROOT'] . '/PHPMailer/src/PHPMailer.php';
+    require $_SERVER['DOCUMENT_ROOT'] . '/PHPMailer/src/SMTP.php';
 
+    // Créer une nouvelle instance de PHPMailer
+    $mail = new PHPMailer();
+
+    // Configurer le serveur SMTP
+    $mail->isSMTP();
+    $mail->Host = 'ssl0.ovh.net';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'ne-pas-repondre@crowdhub.fr';
+    $mail->Password = 'Zeas-56vgfA';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+
+    // Configurer l'expéditeur et le destinataire
+    $mail->setFrom('ne-pas-repondre@crowdhub.fr', 'Crowdhub');
+    foreach ($results as $result) {
+        $mail->addAddress($result['email'], $result['firstname'] . ' ' . $result['lastname']);
+    }
+
+    // Ajouter le sujet et le corps du message
+    $mail->Subject = $title;
+    $mail->msgHTML($content);
--- user/voirprofile.php
+++ user/voirprofile.php
@@ -1,77 +1,75 @@
 <?php
-  session_start();
-  require $_SERVER['DOCUMENT_ROOT'] . "/conf.inc.php";
-  require $_SERVER['DOCUMENT_ROOT'] . "/core/functions.php";
-  $pageTitle = "Connexion";
-  saveLogs();
-  getUserInfos();
-  include $_SERVER['DOCUMENT_ROOT'] . "/assets/templates/header.php";
+session_start();
+require $_SERVER['DOCUMENT_ROOT'] . "/conf.inc.php";
+require $_SERVER['DOCUMENT_ROOT'] . "/core/functions.php";
+$pageTitle = "Connexion";
+saveLogs();
+getUserInfos();
+include $_SERVER['DOCUMENT_ROOT'] . "/assets/templates/header.php";
 ?>
 
+<div class="container-fluid">
+    <?php
+    $connection = connectDB();
+    $profileUserId = $_GET['id'];
+    $currentUserId = $_SESSION['id'];
 
-    <div class="container-fluid">
-        <?php
-        $connection = connectDB();
-        $profileUserId = $_GET['id'];
-        $currentUserId = $_SESSION['id'];
+    try {
+        $stmt = $connection->prepare("SELECT * FROM ".DB_PREFIX."user WHERE id = :id");
+        $stmt->bindParam(':id', $profileUserId);
+        $stmt->execute();
+        $user = $stmt->fetch();
 
-        try {
-            $stmt = $connection->prepare("SELECT * FROM pa_user WHERE id = :id");
-            $stmt->bindParam(':id', $profileUserId);
-            $stmt->execute();
-            $user = $stmt->fetch();
+        if ($user) {
+            echo "Profil de " . htmlspecialchars($user['firstname']) . " " . htmlspecialchars($user['lastname']);
 
-            if ($user) {
-                echo "Profil de " . htmlspecialchars($user['firstname']) . " " . htmlspecialchars($user['lastname']);
+            $friendshipStmt = $connection->prepare("SELECT status, blocked_status FROM ".DB_PREFIX."friendship WHERE (user1_id = :user1_id AND user2_id = :user2_id) OR (user1_id = :user2_id AND user2_id = :user1_id)");
+            $friendshipStmt->bindParam(':user1_id', $currentUserId);
+            $friendshipStmt->bindParam(':user2_id', $profileUserId);
+            $friendshipStmt->execute();
+            $friendship = $friendshipStmt->fetch();
 
-                $friendshipStmt = $connection->prepare("SELECT status, blocked_status FROM friendship WHERE (user1_id = :user1_id AND user2_id = :user2_id) OR (user1_id = :user2_id AND user2_id = :user1_id)");
-                $friendshipStmt->bindParam(':user1_id', $currentUserId);
-                $friendshipStmt->bindParam(':user2_id', $profileUserId);
-                $friendshipStmt->execute();
-                $friendship = $friendshipStmt->fetch();
+            if ($friendship === false) {
+                echo "<form action='friendshipActions.php' method='post'>";
+                echo "<input type='hidden' name='action' value='sendFriendRequest'>";
+                echo "<input type='hidden' name='user_id' value='" . $profileUserId . "'>";
+                echo "<button type='submit'>Ajouter comme ami</button>";
+                echo "</form>";
+            } elseif ($friendship['status'] === 'pending') {
+                echo "Demande en attente";
+            } elseif ($friendship['status'] === 'accepted') {
+                echo "<form action='friendshipActions.php' method='post'>";
+                echo "<input type='hidden' name='action' value='removeFriend'>";
+                echo "<input type='hidden' name='friend_id' value='" . $profileUserId . "'>";
+                echo "<button type='submit'>Supprimer l'ami</button>";
+                echo "</form>";
 
-                if ($friendship === false) {
+                if ($friendship['blocked_status'] === 'blocked') {
+                    // Bouton de déblocage
                     echo "<form action='friendshipActions.php' method='post'>";
-                    echo "<input type='hidden' name='action' value='sendFriendRequest'>";
+                    echo "<input type='hidden' name='action' value='unblockUser'>";
                     echo "<input type='hidden' name='user_id' value='" . $profileUserId . "'>";
-                    echo "<button type='submit'>Ajouter comme ami</button>";
+                    echo "<button type='submit'>Débloquer</button>";
                     echo "</form>";
-                } elseif ($friendship['status'] === 'pending') {
-                    echo "Demande en attente";
-                } elseif ($friendship['status'] === 'accepted') {
+                } else {
+                    // Bouton de blocage
                     echo "<form action='friendshipActions.php' method='post'>";
-                    echo "<input type='hidden' name='action' value='removeFriend'>";
-                    echo "<input type='hidden' name='friend_id' value='" . $profileUserId . "'>";
-                    echo "<button type='submit'>Supprimer l'ami</button>";
+                    echo "<input type='hidden' name='action' value='blockUser'>";
+                    echo "<input type='hidden' name='user_id' value='" . $profileUserId . "'>";
+                    echo "<button type='submit'>Bloquer</button>";
                     echo "</form>";
-
-                    if ($friendship['blocked_status'] === 'blocked') {
-                        // Bouton de déblocage
-                        echo "<form action='friendshipActions.php' method='post'>";
-                        echo "<input type='hidden' name='action' value='unblockUser'>";
-                        echo "<input type='hidden' name='user_id' value='" . $profileUserId . "'>";
-                        echo "<button type='submit'>Débloquer</button>";
-                        echo "</form>";
-                    } else {
-                        // Bouton de blocage
-                        echo "<form action='friendshipActions.php' method='post'>";
-                        echo "<input type='hidden' name='action' value='blockUser'>";
-                        echo "<input type='hidden' name='user_id' value='" . $profileUserId . "'>";
-                        echo "<button type='submit'>Bloquer</button>";
-                        echo "</form>";
-                    }
                 }
-
-                // Bouton pour aller à la messagerie
-                echo "<a href='messagerie.php' class='btn'>Aller à la messagerie</a>";
-            } else {
-                echo "Profil introuvable";
             }
-        } catch (PDOException $e) {
-            echo "Erreur lors de la récupération des informations de profil: " . $e->getMessage();
-        }
-        ?>
-    </div>
 
-    <?php include $_SERVER['DOCUMENT_ROOT']."/assets/templates/footer.php"; ?>
-</main>
+            // Bouton pour aller à la messagerie
+            echo "<a href='messagerie.php' class='btn'>Aller à la messagerie</a>";
+        } else {
+            echo "Profil introuvable";
+        }
+    } catch (PDOException $e) {
+        echo "Erreur lors de la récupération des informations de profil: " . $e->getMessage();
+    }
+    ?>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT']."/assets/templates/footer.php"; ?>
