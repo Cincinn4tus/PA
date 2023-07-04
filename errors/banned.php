d53db3242940484ab8d5f5aba0fe7bb595265487
Cincinn4tus <aligoumane@protonmail.com>
2023-07-04 08:59:19 +0200

    banned

--- errors/banned.php
+++ errors/banned.php
@@ -1,1 +1,14 @@
+<?php
+  require $_SERVER['DOCUMENT_ROOT'] . "/conf.inc.php";
+  require $_SERVER['DOCUMENT_ROOT'] . "/core/functions.php";
+  $pageTitle = "Banni";
+  include $_SERVER['DOCUMENT_ROOT'] . "/assets/templates/header.php";
+?>
+
 <img src="https://www.irit.fr/~Loic.Barthe/images/kevin.png" alt="">
+
+<h1>Tu as été banni</h1>
+
+<p>
+    * PHP est codé en C
+</p>
